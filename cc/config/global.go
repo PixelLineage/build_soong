5724b81d5b597c572d221a09449a3a38580860dd
EmanuelCN <emanuelghub@gmail.com>
2025-09-27 00:39:58 +0530

    soong: Disable stack protector
    Change-Id: Id1e85a075a3b0df526c03ce24c63dd3d421be537

--- cc/config/global.go
+++ cc/config/global.go
@@ -161,7 +161,7 @@ var (
 		"-fdata-sections",
 		"-fno-short-enums",
 		"-funwind-tables",
-		"-fstack-protector-strong",
+		"-fno-stack-protector",
 		"-Wa,--noexecstack",
 		"-D_FORTIFY_SOURCE=2",
 
